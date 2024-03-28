a29de3795612004376e43cad25b3813673c957af
CaptainAlekor <sashakorostelev49@gmail.com>
2024-03-28 11:33:55 +0300

    Проверка работы DBHelper

--- CRUD_Console/src/main/java/Main.java
+++ CRUD_Console/src/main/java/Main.java
@@ -1,5 +1,21 @@
+import tools.DBHelper;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
+        try (DBHelper db = new DBHelper();
+             PreparedStatement statement = db.getConnection().prepareStatement("select * from test_table;");
+        ) {
+            ResultSet result = statement.executeQuery();
 
+            while (result.next()) {
+                System.out.println(result.getInt("value"));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
